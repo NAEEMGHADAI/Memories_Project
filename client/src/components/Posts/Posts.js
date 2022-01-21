20f682cdd8f20148f213a59d0c806a9de45f1f77
NAEEMGHADAI <naeemghadai007.com@gmail.com>
2022-01-21 22:09:26 +0530

    posts imports

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Grid, CircularProgress } from "@material-ui/core";
 import { useSelector } from "react-redux";
 import Post from "./Post/Post";
 import useStyles from "./styles";
@@ -6,12 +7,21 @@ const Posts = () => {
 	const posts = useSelector((state) => state.posts);
 	const classes = useStyles();
 	console.log(posts);
-	return (
-		<>
-			<h1>Posts</h1>
-			<Post />
-			<Post />
-		</>
+	return !posts.lenght ? (
+		<CircularProgress />
+	) : (
+		<Grid
+			className={classes.container}
+			container
+			alignItems="stretch"
+			spacing={3}
+		>
+			{posts.map((post) => (
+				<Grid item xs={12} sm={6} key={post.id}>
+					<Post post={post} />
+				</Grid>
+			))}
+		</Grid>
 	);
 };
 
