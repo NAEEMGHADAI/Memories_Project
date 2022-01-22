40c9dbe1f6e72b8a2be43d4a7af1f3468bff389f
NAEEMGHADAI <naeemghadai007.com@gmail.com>
2022-01-22 18:24:20 +0530

    Post Ui

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 const url = "http://localhost:5000/posts/";
 
-export const fetchPosts = async () => axios.get(url);
-export const createPost = async (newPost) => axios.post(url, newPost);
+export const fetchPosts = () => axios.get(url);
+export const createPost = (newPost) => axios.post(url, newPost);
--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -7,7 +7,8 @@ const Posts = () => {
 	const posts = useSelector((state) => state.posts);
 	const classes = useStyles();
 	console.log(posts);
-	return !posts.lenght ? (
+	console.log(posts.length);
+	return !posts.length ? (
 		<CircularProgress />
 	) : (
 		<Grid
@@ -17,7 +18,7 @@ const Posts = () => {
 			spacing={3}
 		>
 			{posts.map((post) => (
-				<Grid item xs={12} sm={6} key={post.id}>
+				<Grid item xs={12} sm={6} key={post._id}>
 					<Post post={post} />
 				</Grid>
 			))}
@@ -26,3 +27,29 @@ const Posts = () => {
 };
 
 export default Posts;
+
+// import React from 'react';
+// import { Grid, CircularProgress } from '@material-ui/core';
+// import { useSelector } from 'react-redux';
+
+// import Post from './Post/Post';
+// import useStyles from './styles';
+
+// const Posts = ({ setCurrentId }) => {
+//   const posts = useSelector((state) => state.posts);
+//   const classes = useStyles();
+
+//   return (
+//     !posts.length ? <CircularProgress /> : (
+//       <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+//         {posts.map((post) => (
+//           <Grid key={post._id} item xs={12} sm={6} md={6}>
+//             <Post post={post} setCurrentId={setCurrentId} />
+//           </Grid>
+//         ))}
+//       </Grid>
+//     )
+//   );
+// };
+
+// export default Posts;
